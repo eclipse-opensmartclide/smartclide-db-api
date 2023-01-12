5d71c5756ad9f5add303926e759ceed65fe4f6ca
Athanasios Salamanis <athanasios.salamanis@gmail.com>
2023-01-12 12:52:10 +0200

    Proceeded with the process of adding unit tests for the controllers of the API. Still stuck into the MongoDB repositories dependency.

--- src/test/java/org/eclipse/opensmartclide/dbapi/controller/ServiceControllerTest.java
+++ src/test/java/org/eclipse/opensmartclide/dbapi/controller/ServiceControllerTest.java
@@ -4,50 +4,67 @@
 package org.eclipse.opensmartclide.dbapi.controller;
 
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-import org.assertj.core.util.Arrays;
 import org.eclipse.opensmartclide.dbapi.model.Service;
 import org.eclipse.opensmartclide.dbapi.repository.ServiceRepository;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
+
+// Mockito
+import static org.mockito.Mockito.when;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+// WebMvcTest
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 /**
  * @author asal
  *
  */
 @RunWith(SpringRunner.class)
-@WebMvcTest(ServiceController.class)
-class ServiceControllerTest {
+@WebMvcTest(controllers = ServiceController.class)
+public class ServiceControllerTest {
 	
-    @Autowired
+	@MockBean
+    private ServiceRepository serviceRepository;
+	
+	@MockBean
+	private MongoTemplate template;
+   
+	@Autowired
     private MockMvc mvc;
     
-    @MockBean
-    private ServiceRepository repository;
-
 	@Test
-	void getAllServicesEndpoint() throws Exception {
-		
+	public void getAllServicesEndpoint() throws Exception {
+		// mock services repository
 		Service service = new Service();
-		service.setName("TestService");
-		List<Service> services = Arrays.asList(service);
-		repository.saveAll(services);
+		service.setName("TestService-1");
+		List<Service> services = new ArrayList<>(Arrays.asList(service));
+		when(serviceRepository.findAll()).thenReturn(services);
 		
-		mvc.perform(MockMvcRequestBuilders
-				.get("/services")
-				.accept(MediaType.APPLICATION_JSON))
+		String uri = "/services";
+		
+		// perform test
+		mvc.perform(MockMvcRequestBuilders.get(uri)
+			      .accept(MediaType.APPLICATION_JSON_VALUE))
 		.andExpect(MockMvcResultMatchers.status().isOk())
-		.andExpect(MockMvcResultMatchers.jsonPath("$.services").exists())
-		.andExpect(MockMvcResultMatchers.jsonPath("$.services[*].id").isNotEmpty());
-		
+		.andExpect(MockMvcResultMatchers.jsonPath("$.size()").value(services.size()))
+		.andDo(MockMvcResultHandlers.print());	
 	}
-
 }
